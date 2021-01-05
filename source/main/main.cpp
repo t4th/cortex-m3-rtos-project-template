30488e39112d7ea9f1f736f56310c726f3008012
tath <tath@o2.pl>
2021-01-05 18:53:38 +0100

    Update project.

--- source/main/main.cpp
+++ source/main/main.cpp
@@ -1,16 +1,16 @@
-// cortex-m3-rtos template project
+// Cortex-M3-rtos template project.
 #include <kernel.hpp>
 
-void task_routine(void * a_parameter)
+void task_routine( void * a_parameter)
 {
-    while(1);
+    while( true);
 }
 
 int main()
 {
     kernel::init();
 
-    kernel::task::create(task_routine, kernel::task::Priority::Idle);
+    kernel::task::create( task_routine, kernel::task::Priority::Idle);
 
     kernel::start();
 
